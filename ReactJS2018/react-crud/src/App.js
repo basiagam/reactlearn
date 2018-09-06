246e84191905edb7353e31f119ddd854f439f27c
Marcin Basiaga <basiagam@yahoo.com>
2018-09-06 12:58:47 +0200

    react-crud add todo

---
+++ ReactJS2018/react-crud/src/App.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      newTodo: '',
+      todos: [{
+          id: 1, name: 'Buy milk'
+        },{
+          id: 2, name: 'Buy some clothes'
+        },
+        {
+          id: 3, name: 'Fix car'
+        },
+        {
+          id: 4, name: 'Pay electrical bills'
+      }]
+    };
+
+    //bindujemy aby manipulowac state
+    this.handleChange = this.handleChange.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  handleChange(event){
+    this.setState({
+        newTodo:event.target.value
+    });
+  }
+
+  addTodo(){
+    const newTodo = {
+        name: this.state.newTodo,
+        id: this.state.todos[this.state.todos.length - 1].id +1
+    };
+
+    //kopiujemy todos ze state do zmiennej i dodajemy nowy todo
+    const todos = this.state.todos;
+    todos.push(newTodo);
+
+    this.setState({
+      todos: todos,
+      newTodo: ''
+    });
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">CRUD React</h1>
+        </header>
+        <div className="container">
+          <input 
+          type="text"
+          name="todo" 
+          className="my-4 form-control"
+          placeholder="Add a new todo"
+          onChange={this.handleChange}
+          value={this.state.newTodo}
+          />
+
+          <button 
+          onClick={this.addTodo}
+          className="btn-info mb-3 form-control"
+          >Add todo</button>
+
+          <ul className="list-group">
+            {this.state.todos.map((item) =>{
+              return <li key={item.id} className="list-group-item">{item.name}</li>
+            })}
+          </ul>
+        </div>
+      </div> 
+    );
+  }
+}
+
+export default App;
