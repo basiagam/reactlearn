2022fe6d46d24d18affdcfc13ecd5831d31a2573
Marcin Basiaga <marcin.basiaga@wisniowski.pl>
2018-09-11 11:25:50 +0200

    pociecie szablonu statycznego na componenty

--- ReactJS2018/react-blog/src/index.js
+++ ReactJS2018/react-blog/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 
 
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Welcome from './components/Welcome';
 import Footer from './components/Footer';
+import CreateArticle from './components/CreateArticle';
+import Login from './components/Login';
+import SingleArticle from './SingleArticle';
 
 const Homepage = () => {
     return <h1>This is the homepage.</h1>
@@ -22,8 +25,9 @@ ReactDOM.render(
         <div>
             <Navbar />
             <Route exact path="/" component={Welcome} />
-            <Route path="/about" component={About} />
-            <Route path="/home" component={Homepage} />
+            <Route path="/login" component={Login} />
+            <Route path="/article/:slug" component={SingleArticle} />
+            <Route path="/articles/create" component={CreateArticle} />
             <Footer />
         </div>  
     </BrowserRouter>
